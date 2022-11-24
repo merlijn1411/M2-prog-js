68401714569d49913cf9b0609bee823c33eb227b
merlijn1411 <34530@ma-web.nl>
2022-11-24 10:27:08 +0100

    les1 vb

---
+++ les1/Aap.js
@@ -0,0 +1,17 @@
+class Aap1
+{
+    runApplication()
+    {
+        let appnaam = "WEB"
+        let versienummer = "98701"
+        const versiedatum = ("2022-11-24");
+        let Auther = "Merlijn van Aalst"
+        let dopyright = "SD1A"
+        let distributeur = "Aalst"
+        let darkmode = 
+        console.log(appnaam, versienummer, versiedatum, Auther, dopyright, distributeur)
+    }
+}
+
+let Aap = new Aap1();
+Aap.runApplication();
