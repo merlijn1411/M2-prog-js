5f4556b49b5629f18e0f4f9b5fc4a73aa7f55449
merlijn1411 <34530@ma-web.nl>
2022-12-07 13:08:00 +0100

    les1 verbetering

--- les1/app.js
+++ les1/app.js
@@ -2,7 +2,12 @@ class App
 {
     runApplication()
     {
-        let canvas = document.getElementById("")
+        let eenVariableNaam = "hello"
+        let Appnaam = "ditbenik"
+        let versienummer = "98701"
+        let versie_datum = "2022-11-24"
+        let autheur = "Merlijn van Aalst"
+        let 
     }
 }
 
