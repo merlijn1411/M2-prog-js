641205c732ac4d63c5d8990a5300032d3b43e631
merlijn1411 <34530@ma-web.nl>
2022-12-07 13:48:40 +0100

    les 2 huis met raam

--- les2/funcionts/app.js
+++ les2/funcionts/app.js
@@ -8,8 +8,8 @@ class App
         g.canvas.height = window.innerHeight;
         var black = "#000000";
         var white = "#FFFFFF";
-        var red = "#ff0000";
-
+        var Yellow = "#FFFF00";
+        //roof
         g.beginPath();
         g.moveTo(300,100);
         g.lineTo(700,200);
@@ -21,18 +21,19 @@ class App
         g.fill();
         g.stroke();
         
-
+        //side 
         g.beginPath();
         g.moveTo(200,300);
         g.lineTo(200,500);
         g.lineTo(600,600);
         g.lineTo(600,400);
         g.lineTo(200,300);
-        g.fillStyle = red;
+        g.fillStyle = white;
         g.fill();
         g.stroke();
         g.closePath();
-
+        
+        //front
         g.beginPath();
         g.fillStyle = white;
         g.moveTo(600, 400);
@@ -44,6 +45,19 @@ class App
         g.closePath();
         g.stroke();
         g.fill();
+        
+        //window
+        g.beginPath();
+        g.fillStyle = Yellow;
+        g.moveTo(450, 500);
+        g.lineTo(500, 510);
+        g.lineTo(510, 400);
+        g.lineTo(400, 375);
+        g.lineTo(390, 485);
+        g.closePath();
+        g.stroke();
+        g.fill();
+    
     }
 
 }
