bf1de45626563b00576165850ae4031a2fcd300b
merlijn1411 <34530@ma-web.nl>
2022-12-13 10:46:49 +0100

    canvashuis benodigdheden

--- canvashuis/app.js
+++ canvashuis/app.js
@@ -9,13 +9,13 @@ class App
     var g = canvas.getContext('2d');
     g.canvas.width  = window.innerWidth;
     g.canvas.height = window.innerHeight;
-    var g = canvas.width / 2;
-    var g = canvas.height / 2;
-    var black = "#735F32";
-    var white = "#ffffff";
-    var blue = "#0000cc";
+    let randomGetal = Math.random();
+    var black = "#000000";
+    var white = "#FFFFFF";
+    var Yellow = "#FFFF00";
     
     
+    //roof
     g.beginPath();
     g.moveTo(300,100);
     g.lineTo(700,200);
@@ -23,21 +23,25 @@ class App
     g.lineTo(200,300);
     g.lineTo(300,100);
     g.fillStyle = black;
-    g.fill();
     g.closePath();
+    g.fill();
+    g.stroke();
     
+    //side 
     g.beginPath();
     g.moveTo(200,300);
     g.lineTo(200,500);
     g.lineTo(600,600);
     g.lineTo(600,400);
     g.lineTo(200,300);
-    g.fillStyle = black;
+    g.fillStyle = white;
     g.fill();
+    g.stroke();
     g.closePath();
     
+    //front
     g.beginPath();
-    g.fillStyle = black;
+    g.fillStyle = white;
     g.moveTo(600, 400);
     g.lineTo(600, 600);
     g.lineTo(800, 500);
@@ -46,7 +50,39 @@ class App
     g.lineTo(600, 400);
     g.closePath();
     g.stroke();
-    g.fill(); 
+    g.fill();
+    
+    //window right
+    g.beginPath();
+    g.fillStyle = Yellow;
+    g.moveTo(450, 500);
+    g.lineTo(500, 510);
+    g.lineTo(510, 400);
+    g.lineTo(400, 375);
+    g.lineTo(390, 485);
+    g.closePath();
+    g.stroke();
+    g.fill();
+
+    //window left
+    g.beginPath();
+    g.fillStyle = white;
+    g.moveTo(385, 370);
+    g.lineTo(300, 350);
+    g.lineTo(290, 450);
+    g.lineTo(380, 480)
+    g.closePath();
+    g.stroke();
+    g.fill();
+
+    //stroke front 
+    g.beginPath();
+    g.moveTo(600, 400);
+    g.lineTo(600, 400);
+    g.lineTo(800, 300);
+    g.closePath();
+    g.stroke();
+    g.fill();
     }
 }
 
