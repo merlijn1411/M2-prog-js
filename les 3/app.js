ff5fe9075fa9a74a8ae999ad4560b90e2a72616c
merlijn1411 <34530@ma-web.nl>
2022-12-22 12:47:30 +0100

    les 3 toevoeging

---
+++ les 3/app.js
@@ -0,0 +1,34 @@
+class App
+{
+    runApplication()
+    {
+        //dit is de ifelse les 
+        //dit is code commentaar voor javascript
+        let randomGetal = Math.random(0,1);
+        
+        var red = "#FF0000";
+        var black = "#000000";
+        var blue = "#0000FF";
+
+       if( 0 < 0,2){
+        document.body.style.background = red;
+       }
+
+       else if( 0 == 0 ){
+        document.body.style.background = black;
+       }
+
+       else if ( 0 > 0.6) {
+        ocument.body.style.background = blue;
+       }
+
+        
+        
+        console.log(randomGetal);
+
+
+    }
+}
+
+let app = new App();
+app.runApplication();
