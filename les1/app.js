fc9517fd3aec0f2c79f1a94f6b97971e71ef9399
merlijn1411 <34530@ma-web.nl>
2022-12-22 11:10:53 +0100

    les 1 verbetering

--- les1/app.js
+++ les1/app.js
@@ -3,8 +3,24 @@ class App
     runApplication()
     {
         
+
+        let AppDate = "24-11-2022";
+        let VNumber = "versienummer: 98701";
+        let AppNaaam = "WEB";
+        let Autheur = "Autheur: Merlijn van Aalst";
+        let copyright = "Copyright = @34530" ;
+        let dis = "Merlijn/34530";
+        const darkMode = window.matchMedia("(prefers-color-scheme:dark)").matches;
+        
+        //dit is de lijst netjes onder elkaar 
+        var Data = [AppDate, VNumber, AppNaaam, Autheur, copyright, dis, darkMode];
+        Data.forEach(function(onderElkaar){
+            console.log(onderElkaar)
+        }) 
     }
 }
+
+
 
 let app = new App();
 app.runApplication();
